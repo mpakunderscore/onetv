fbc207c4ce4e272d0b7f367990018fa2e338b81b
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-08-22 20:53:40 +0300

    Stable

--- index.js
+++ index.js
@@ -9,23 +9,10 @@ function playVideo() {
     player = spawn('sh', ['player.sh', 'video.pm4'], {detached: true});
 }
 
-exports.pins[id].interface = exports.pins[id].type ? new Gpio(exports.pins[id].pid, 'out') : new Gpio(exports.pins[id].pid, 'in', 'both');
+let pinOut = new Gpio(5, 'out');
+let pinIn = new Gpio(6, 'in', 'both');
 
-if (!exports.pins[id].type) {
-
-    exports.pins[id].interface.watch(function (err, value) {
-
-        exports.pins[id].state = (value === 1);
-
-        api.broadcastState(id);
-
-        // console.log(exports.pins[id].id + ' | ' + exports.pins[id].pid + ' | ' + value + ' | ' + exports.pins[id].state);
-
-        scenario.checkScenario();
-    })
-
-} else {
-
-    // console.log(exports.pins[id]);
-    exports.pins[id].interface.writeSync(exports.pins[id].state ? 1 : 0);
-}
+pinIn.watch(function (err, value) {
+    if (value === 1)
+        playVideo();
+})
